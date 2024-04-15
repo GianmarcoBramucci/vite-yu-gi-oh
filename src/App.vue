c916acb414e8db3b261eec22781fd15e6b276979
GianmarcoBramucci <giangian797@gmail.com>
2024-04-15 16:59:50 +0200

    inplamente api and setup

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import { data } from './data';
+import axios from 'axios';
+  export default {
+    name: 'App',
+    data(){
+      return{
+        data
+      }
+    },
+    components:{
+
+    },
+    created(){
+      axios.get(this.data.apiUrl).then((res)=>{
+        this.data.cardsNum= res.data.meta.current_rows;
+        this.data.cards = res.data.data;
+        console.log(this.data.cardsNum);
+        console.log(this.data.cards);
+      })
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
